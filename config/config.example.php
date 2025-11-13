6d57ccfe6c210a6fc58e7e20bb83ca1c4c6a4f72
Manoj M <gowdamanoj402@gmail.com>
2025-11-13 16:54:40 +0530

    Add config.example and update .gitignore (do not track local config)

---
+++ config/config.example.php
@@ -0,0 +1,17 @@
+﻿<?php
+// config/config.example.php
+// Copy to config.inc.php and update values locally. DO NOT commit config.inc.php
+
+\ = array();
+\['db_server']   = '127.0.0.1';
+\['db_database'] = 'dvwa';
+\['db_user']     = 'root';
+\['db_password'] = '';
+\['db_port']     = '3306';
+
+\['recaptcha_public_key']  = '';
+\['recaptcha_private_key'] = '';
+\['default_security_level'] = 'impossible';
+\['default_locale'] = 'en';
+\['disable_authentication'] = false;
+?>
